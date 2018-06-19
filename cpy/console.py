005d6e1cff24bdf2ddebca7769320a079bacaf8e
Mark Fornace <mfornace@caltech.edu>
2018-06-19 09:38:34 -0700

    Fix capturing

--- cpy/console.py
+++ cpy/console.py
@@ -28,9 +28,8 @@ class ConsoleHandler:
 ################################################################################
 
 class ConsoleTestHandler:
-    def __init__(self, index, info, file, footer='\n', indent='    ', format_scope=None):
+    def __init__(self, index, info, file, indent='    ', format_scope=None):
         self.format_scope = format_scope
-        self.footer = footer
         self.indent = indent
         self.index = index
         self.info = info
@@ -39,30 +38,28 @@ class ConsoleTestHandler:
     def write(self, *args):
         for a in args:
             self.file.write(a)
-
-    def __call__(self, event, scopes, logs):
-        self.file.write(readable_message(events(True)[event], scopes, logs, self.indent, self.format_scope))
-        self.file.write(self.footer)
         self.file.flush()
 
+    def __call__(self, event, scopes, logs):
+        self.write(readable_message(events(True)[event], scopes, logs, self.indent, self.format_scope), '\n')
+
     def finalize(self, counts, out, err):
-        if out:
-            self.write('Contents of std::cout:\n', '-' * 22, '\n', out, '-' * 22, '\n\n')
-        if err:
-            self.write('Contents of std::cerr:\n', '-' * 22, '\n', err, '-' * 22, '\n\n')
+        for o, s in zip((out, err), ('cout', 'cerr')):
+            if o:
+                self.write(colored('Contents of std::%s' % s, 'magenta'), ':\n',
+                       '=' * 22, '\n', o, '=' * 22, '\n\n')
         if any(counts):
             s = ', '.join('%s: %d' % (e, c) for e, c in zip(events(True), counts) if c)
-            self.file.write('Counts for test %d: {%s}\n' % (self.index, s))
+            self.write('Counts for test %d: {%s}\n' % (self.index, s))
 
     def __enter__(self):
         if self.info[1]:
             info = repr(self.info[0]) + ' (%s:%d): ' % self.info[1:3] + repr(self.info[3])
         else:
             info = repr(self.info[0])
-        self.file.write('=' * 80)
-        self.file.write(colored('\nTest %d' % self.index, 'blue', attrs=['bold']))
-        self.file.write(': {}\n\n'.format(info))
-        self.file.flush()
+        self.write('=' * 80,
+                   colored('\nTest %d' % self.index, 'blue', attrs=['bold']),
+                   ': {}\n\n'.format(info))
         return self
 
     def __exit__(self, value, cls, traceback):
