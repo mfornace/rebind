e5b55077e756520b357439c5d873123dc4086266
Mark Fornace <mfornace@caltech.edu>
2019-04-23 11:12:56 -0700

    Add Config object to __init__

--- cpy/__init__.py
+++ cpy/__init__.py
@@ -1,6 +1,30 @@
 
+
 class ConversionError(TypeError):
     '''Default error type for all cpy type conversion errors'''
+
+
+################################################################################
+
+class Config:
+    def __init__(self, methods):
+        self._set_debug = methods['set_debug']
+        self._get_debug = methods['debug']
+        self.set_type_error = methods['set_type_error']
+        self.set_type_names = methods['set_type_names']
+        self.set_type = methods['set_type']
+        self.set_output_conversion = methods['set_output_conversion']
+        self.set_input_conversion = methods['set_input_conversion']
+
+    @property
+    def debug(self):
+        return self._get_debug().cast(bool)
+
+    @debug.setter
+    def debug(self, value):
+        self._set_debug(bool(value))
+
+################################################################################
 
 from .render import render_module, render_init, render_member, \
                     render_type, render_object, render_function
--- cpy/render.py
+++ cpy/render.py
@@ -1,28 +1,8 @@
 import inspect, importlib, functools, logging, typing, atexit, collections
-from . import common, ConversionError
+from . import Config, common, ConversionError
 
 log = logging.getLogger(__name__)
 # logging.basicConfig(level=logging.INFO)
-
-################################################################################
-
-class Config:
-    def __init__(self, methods):
-        self._set_debug = methods['set_debug']
-        self._get_debug = methods['debug']
-        self.set_type_error = methods['set_type_error']
-        self.set_type_names = methods['set_type_names']
-        self.set_type = methods['set_type']
-        self.set_output_conversion = methods['set_output_conversion']
-        self.set_input_conversion = methods['set_input_conversion']
-
-    @property
-    def debug(self):
-        return self._get_debug().cast(bool)
-
-    @debug.setter
-    def debug(self, value):
-        self._set_debug(bool(value))
 
 ################################################################################
 
